bbd56eaf7af468594eb02e490918d9ba6bb27f35
Andrew Ruzavin <andrew.ruzavin@gmail.com>
2026-08-21 12:41:44 +0700

    [Feature] Add ShoppingItem model with mock

---
+++ ShoppingList37a/Models/ShoppingItem+Mock.swift
@@ -0,0 +1,10 @@
+//
+//  Untitled.swift
+//  ShoppingList37a
+//
+//  Created by Andrew Ruzavin on 20/08/2026.
+//
+
+extension ShoppingItem {
+    static let mock = ShoppingItem(name: "Чайник", quantity: 2)
+}
