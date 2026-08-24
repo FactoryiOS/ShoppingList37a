534433937c90eaeb5a437e5e8df7efaf2b2fe271
Victor <viktorrr398@gmail.com>
2026-08-24 21:47:59 +0400

    refactor: fix fonts, backgrounds and nest views per review

---
+++ ShoppingList37a/Screens/CreateList/CreateListView+Observed.swift
@@ -0,0 +1,54 @@
+import SwiftUI
+
+extension CreateListView {
+	
+	enum Mode {
+		case create
+		case edit
+		
+		var title: String {
+			switch self {
+			case .create: "Создать список"
+			case .edit: "Редактировать список"
+			}
+		}
+		
+		var actionButtonTitle: String {
+			switch self {
+			case .create: "Создать"
+			case .edit: "Сохранить"
+			}
+		}
+	}
+
+	@MainActor
+	@Observable
+	final class Observed {
+		let mode: Mode
+		var listName: String
+		var selectedColor: SelectableColor?
+		var selectedIcon: SelectableIcon?
+		
+		var isSaveEnabled: Bool {
+			!listName.trimmingCharacters(in: .whitespacesAndNewlines).isEmpty
+			&& selectedColor != nil
+			&& selectedIcon != nil
+		}
+		
+		var currentSelectionColor: Color {
+			selectedColor?.color ?? Color(.slAccent)
+		}
+		
+		init(
+			mode: Mode = .create,
+			listName: String = "",
+			selectedColor: SelectableColor? = nil,
+			selectedIcon: SelectableIcon? = nil
+		) {
+			self.mode = mode
+			self.listName = listName
+			self.selectedColor = selectedColor
+			self.selectedIcon = selectedIcon
+		}
+	}
+}
