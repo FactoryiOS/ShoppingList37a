2c674c66ffa1c8c3c0bd41c3a6bb4c6167457598
Andrew Ruzavin <andrew.ruzavin@gmail.com>
2026-09-01 20:46:54 +0700

    [Bug] Apply theme at root view so switching updates live

--- ShoppingList37a/App/RootView.swift
+++ ShoppingList37a/App/RootView.swift
@@ -8,14 +8,21 @@
 import SwiftUI
 
 struct RootView: View {
-    
+
     @Environment(AppState.self) private var appState
-    
+    @AppStorage(AppTheme.storageKey) private var selectedTheme: AppTheme = .system
+
     var body: some View {
+        content
+            .preferredColorScheme(selectedTheme.colorScheme)
+    }
+
+    @ViewBuilder
+    private var content: some View {
         switch appState.root {
         case .welcome:
             WelcomeScreen(onStart: appState.completeWelcome)
-            
+
         case .main:
             ContentView()
         }
